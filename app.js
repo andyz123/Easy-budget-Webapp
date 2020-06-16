63ae594881b36eebab3e71e3a131236a247562b5
Andy <andyzheng94@live.com>
2020-06-16 02:03:08 -0400

    Update app.js

--- app.js
+++ app.js
@@ -13,3 +13,24 @@ app.use(express.static('public'));
 app.use(favicon(__dirname + '/public/faviconandy.ico'));
 
 mongoose.connect('mongodb://localhost:27017/budgetappDB', { useNewUrlParser: true })
+
+const budgetModel = {
+ 	name: {
+ 		type: String,
+ 		unique: true
+ 	},
+ 	price: Number,
+ 	quantity: Number,
+	sum: Number
+};
+
+const Budget = mongoose.model('Budget', budgetModel);
+
+app.get('/', function(req, res){
+	let date = new Date();
+
+	let options = {
+		weekday: 'long', 
+		day: 'numeric', 
+		month: 'long'
+	};
